83a946b40e17f066dc9cc5ed578ee83b281f9487
agilan2001 <agilanvlr2001@gmail.com>
2021-12-16 00:44:28 +0530

    update images

--- public/js/events.js
+++ public/js/events.js
@@ -8,11 +8,11 @@ fetch("data/event_details.json").then((res) => res.json()).then((event_data) => 
     for (event_categ in event_data) {
         var categ_inner = "";
         for (tech_event in event_data[event_categ]) {
-            categ_inner += `<div class="card m-3" style="width: 18rem;">
+            categ_inner += `<div class="card m-3 event-card" style="width: 18rem;">
             <img src="images/events/${event_data[event_categ][tech_event]['img']}.jpg" class="card-img-top">
                 <div class="card-body">
                     <h5 class="card-title">${tech_event}</h5>
-                    <p class="card-text mb-5">${event_data[event_categ][tech_event]["short_desc"]}</p>
+                    <p class="card-text">${event_data[event_categ][tech_event]["short_desc"]}</p>
                     <button onclick="register_clk('${tech_event}','${event_categ}')" class="form-control" id="submit-button">REGISTER</button>
                 </div>
             </div>`
